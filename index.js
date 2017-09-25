5b5538294d0d777a6b82f529c6da215daf6be7a3
axetroy <troy450409405@gmail.com>
2017-09-25 14:53:44 +0800

    update

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const PORT = +(process.env.PORT || 3004);
   console.info(`Listen on ${PORT}`);
   http
     .createServer(function(req, res) {
-      response.writeHead(200, { 'Content-Type': 'text/plain' });
-      response.write("I am Axetroy' miner, I am digesting...");
-      response.end();
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.write("Hi, I am Axetroy' miner, I am digesting...");
+      res.end();
     })
     .listen(PORT, 'localhost');
 
