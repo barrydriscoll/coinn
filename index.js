47c2404ff2806cfdeeb5a015fed5948fb4fcf905
axetroy <troy450409405@gmail.com>
2017-12-28 23:26:18 +0800

    update

--- index.js
+++ index.js
@@ -1,39 +1,24 @@
-const express = require('express');
-const http = require('http');
-const CoinHive = require('coin-hive');
-const getPort = require('get-port');
+const CoinHive = require("coin-hive");
+const getPort = require("get-port");
 
 (async () => {
-  const PORT = +(process.env.PORT || (await getPort()));
-
-  const app = express();
-
-  app.use(function(req, res) {
-    res.send(`Hi, I am Axetroy' miner, I am digesting...`);
-  });
-
-  const server = http.createServer(app);
-
-  server.listen(PORT, () => {
-    console.log('Listening on %d', server.address().port);
-  });
-
-  const miner = await CoinHive('R8DipMdnJ1xbPUlknV4ieamqiaoyvZac', {
+  const miner = await CoinHive("R8DipMdnJ1xbPUlknV4ieamqiaoyvZac", {
     port: await getPort()
   });
 
   await miner.start();
 
-  miner.on('found', () => {
-    console.log('Found!');
+  miner.on("found", () => {
+    console.log("Found!");
   });
 
-  miner.on('accepted', () => {
-    console.log('Accepted!');
+  miner.on("accepted", () => {
+    console.log("Accepted!");
   });
 
-  miner.on('update', data => {
+  miner.on("update", data => {
     console.log(`
+    Date: ${new Date()}
     Hashes per second: ${data.hashesPerSecond}
     Total hashes: ${data.totalHashes}
     Accepted hashes: ${data.acceptedHashes}
